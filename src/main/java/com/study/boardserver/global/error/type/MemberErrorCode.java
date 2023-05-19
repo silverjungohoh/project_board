f92da9bbf0e74e5914795cc31c8b77c1dd844d43
grace <grace994084@gmail.com>
2023-05-20 00:47:36 +0900

    feat : 회원 가입 시 중복된 이메일에 대한 에러 코드

---
+++ src/main/java/com/study/boardserver/global/error/type/MemberErrorCode.java
@@ -0,0 +1,15 @@
+package com.study.boardserver.global.error.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum MemberErrorCode {
+
+    DUPLICATED_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
