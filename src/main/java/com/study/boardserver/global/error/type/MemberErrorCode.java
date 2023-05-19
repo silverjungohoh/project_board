c18f1781aabe23ef494cdd71fd9167a9a2ca698d
grace <grace994084@gmail.com>
2023-05-20 00:59:31 +0900

    feat : 회원 가입 시 중복된 닉네임에 대한 에러 코드

--- src/main/java/com/study/boardserver/global/error/type/MemberErrorCode.java
+++ src/main/java/com/study/boardserver/global/error/type/MemberErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum MemberErrorCode {
 
-    DUPLICATED_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
+    DUPLICATED_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+    DUPLICATED_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다.");
 
     private final HttpStatus status;
     private final String message;
