04f3f231b8092a8fe22ced45a647b6c8bbe86ba8
grace <grace994084@gmail.com>
2023-05-20 16:05:13 +0900

    test : 이메일 및 닉네임 중복 확인 api 구현

---
+++ src/main/java/com/study/boardserver/domain/member/controller/MemberController.java
@@ -0,0 +1,36 @@
+package com.study.boardserver.domain.member.controller;
+
+import com.study.boardserver.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+@Tag(name = "회원", description = "회원 API Document")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/sign-up/email")
+    @Operation(summary = "이메일 중복 확인")
+    public ResponseEntity<Map<String, String>> checkDuplicatedEmail(@RequestBody Map<String, String> email) {
+        Map<String, String> result = memberService.checkDuplicatedEmail(email.get("email"));
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/nickname")
+    @Operation(summary = "닉네임 중복 확인")
+    public ResponseEntity<Map<String, String>> checkDuplicatedNickname(@RequestBody Map<String, String> nickname) {
+        Map<String, String> result = memberService.checkDuplicatedNickname(nickname.get("nickname"));
+        return ResponseEntity.ok(result);
+    }
+}
