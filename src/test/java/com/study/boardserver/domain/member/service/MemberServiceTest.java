d92e3998a1f9abaab8860dd247831c336544cec0
grace <grace994084@gmail.com>
2023-05-20 13:12:09 +0900

    test : 이메일 및 닉네임 중복 확인 기능 테스트 코드

---
+++ src/test/java/com/study/boardserver/domain/member/service/MemberServiceTest.java
@@ -0,0 +1,92 @@
+package com.study.boardserver.domain.member.service;
+
+import com.study.boardserver.domain.member.entity.Member;
+import com.study.boardserver.domain.member.repository.MemberRepository;
+import com.study.boardserver.global.error.exception.MemberException;
+import com.study.boardserver.global.error.type.MemberErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceTest {
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @InjectMocks
+    private MemberServiceImpl memberService;
+
+    @Test
+    @DisplayName("이메일 중복 확인 - 중복 없음")
+    void checkDuplicatedEmail_No() {
+        String email = "test@test.com";
+        // given
+        given(memberRepository.findByEmail(anyString())).willReturn(Optional.empty());
+        // when
+        Map<String, String> result = memberService.checkDuplicatedEmail(email);
+        // then
+        assertNotNull(result.get("message"));
+    }
+
+    @Test
+    @DisplayName("이메일 중복 확인 - 중복 있음")
+    void checkDuplicatedEmail_Yes() {
+        String email = "test@test.com";
+        Member member = Member.builder()
+                .id(1L)
+                .email("test@test.com")
+                .nickname("nick")
+                .build();
+
+        // given
+        given(memberRepository.findByEmail(anyString())).willReturn(Optional.of(member));
+        // when
+        MemberException exception = assertThrows(MemberException.class,
+                () -> memberService.checkDuplicatedEmail(email));
+        // then
+        assertEquals(exception.getErrorCode(), MemberErrorCode.DUPLICATED_EMAIL);
+    }
+
+    @Test
+    @DisplayName("닉네임 중복 확인 - 중복 없음")
+    void checkDuplicatedNickname_No() {
+        String nick = "nick";
+        // given
+        given(memberRepository.findByNickname(anyString())).willReturn(Optional.empty());
+        // when
+        Map<String, String> result = memberService.checkDuplicatedNickname(nick);
+        // then
+        assertNotNull(result.get("message"));
+    }
+
+    @Test
+    @DisplayName("닉네임 중복 확인 - 중복 있음")
+    void checkDuplicatedNickname_Yes() {
+        String nick = "nick";
+        Member member = Member.builder()
+                .id(1L)
+                .email("test@test.com")
+                .nickname("nick")
+                .build();
+
+        // given
+        given(memberRepository.findByNickname(anyString())).willReturn(Optional.of(member));
+
+        // when
+        MemberException exception = assertThrows(MemberException.class,
+                () -> memberService.checkDuplicatedNickname(nick));
+        // then
+        assertEquals(exception.getErrorCode(), MemberErrorCode.DUPLICATED_NICKNAME);
+    }
+}
