e676fb93fcb85f23af5dcbb3eb2a5a7f7ce2a268
grace <grace994084@gmail.com>
2023-05-21 22:32:21 +0900

    feat : 회원 이메일 인증 코드가 유효하지 않는 경우에 대한 에러 코드

--- src/main/java/com/study/boardserver/global/error/type/MemberErrorCode.java
+++ src/main/java/com/study/boardserver/global/error/type/MemberErrorCode.java
@@ -10,7 +10,8 @@ public enum MemberErrorCode {
 
     DUPLICATED_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
     DUPLICATED_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
-    FAIL_TO_SEND_EMAIL(HttpStatus.BAD_REQUEST, "이메일 전송에 실패하였습니다.");
+    FAIL_TO_SEND_EMAIL(HttpStatus.BAD_REQUEST, "이메일 전송에 실패하였습니다."),
+    INVALID_EMAIL_AUTH_CODE(HttpStatus.BAD_REQUEST, "이메일 인증 코드가 유효하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
