9aeee14f5f65744c23f9dd4b0dc4c4e6820c562a
grace <grace994084@gmail.com>
2023-05-22 16:42:04 +0900

    test : redis 테스트 수정

--- src/test/java/com/study/boardserver/config/EmbeddedRedisConfig.java
+++ src/test/java/com/study/boardserver/config/EmbeddedRedisConfig.java
@@ -5,17 +5,15 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.util.StringUtils;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 
 @TestConfiguration
+@EnableRedisRepositories
 public class EmbeddedRedisConfig {
 
     @Value("${spring.redis.port}")
@@ -32,9 +30,8 @@ public class EmbeddedRedisConfig {
     }
 
     @PostConstruct
-    public void redisServer() throws IOException {
-        int port = isRedisRunning()? findAvailablePort() : redisPort;
-        redisServer = new RedisServer(port);
+    public void redisServer() {
+        redisServer = new RedisServer(redisPort);
         redisServer.start();
     }
 
@@ -43,57 +40,5 @@ public class EmbeddedRedisConfig {
         if (redisServer != null) {
             redisServer.stop();
         }
-    }
-
-    /**
-     * Embedded Redis가 현재 실행중인지 확인
-     */
-    private boolean isRedisRunning() throws IOException {
-        return isRunning(executeGrepProcessCommand(redisPort));
-    }
-
-    /**
-     * 현재 PC/서버에서 사용가능한 포트 조회
-     */
-    public int findAvailablePort() throws IOException {
-
-        for (int port = 10000; port <= 65535; port++) {
-            Process process = executeGrepProcessCommand(port);
-            if (!isRunning(process)) {
-                return port;
-            }
-        }
-
-        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
-    }
-
-    /**
-     * 해당 port를 사용중인 프로세스 확인하는 sh 실행
-     */
-    private Process executeGrepProcessCommand(int port) throws IOException {
-
-        String command = String.format("netstat -ano | find \"LISTEN\" | find \"%d\"", port);
-        String[] shell = {"cmd.exe", "/y", "/c", command};
-
-        return Runtime.getRuntime().exec(shell);
-    }
-
-    /**
-     * 해당 Process가 현재 실행중인지 확인
-     */
-    private boolean isRunning(Process process) {
-        String line;
-        StringBuilder pidInfo = new StringBuilder();
-
-        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-
-            while ((line = input.readLine()) != null) {
-                pidInfo.append(line);
-            }
-
-        } catch (Exception e) {
-        }
-
-        return StringUtils.hasText(pidInfo.toString());
     }
 }
