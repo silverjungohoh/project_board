a1434ee8e6b4470e9cec615db443b9843b011160
grace <grace994084@gmail.com>
2023-05-25 17:46:39 +0900

    feat : AuthenticationManager 빈 생성

--- src/main/java/com/study/boardserver/domain/security/config/SecurityConfig.java
+++ src/main/java/com/study/boardserver/domain/security/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.study.boardserver.domain.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -15,6 +17,11 @@ import org.springframework.web.cors.CorsUtils;
 @EnableWebSecurity
 public class SecurityConfig {
 
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
