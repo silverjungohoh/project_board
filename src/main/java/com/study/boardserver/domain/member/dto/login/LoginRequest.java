39ee78f73e29401697205351a8919f71921b62ac
grace <grace994084@gmail.com>
2023-05-26 21:57:06 +0900

    feat : 로그인 요청 및 응답 dto 생성

---
+++ src/main/java/com/study/boardserver/domain/member/dto/login/LoginRequest.java
@@ -0,0 +1,21 @@
+package com.study.boardserver.domain.member.dto.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "아이디는 입력하세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    private String password;
+}
