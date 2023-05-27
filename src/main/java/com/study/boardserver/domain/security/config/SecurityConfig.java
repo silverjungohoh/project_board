d3b9e015b60a253097b0df7521e0eb922b97ed27
grace <grace994084@gmail.com>
2023-05-27 23:50:45 +0900

    feat : security config 추가

--- src/main/java/com/study/boardserver/domain/security/config/SecurityConfig.java
+++ src/main/java/com/study/boardserver/domain/security/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.study.boardserver.domain.security.config;
 
+import com.study.boardserver.domain.security.handler.CustomAccessDeniedHandler;
+import com.study.boardserver.domain.security.handler.CustomAuthenticationEntryPoint;
+import com.study.boardserver.domain.security.jwt.JwtAuthenticationFilter;
+import com.study.boardserver.domain.security.jwt.JwtExceptionFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,11 +16,20 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsUtils;
+
+import static com.study.boardserver.domain.member.type.MemberRole.ROLE_ADMIN;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtExceptionFilter jwtExceptionFilter;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
 
     @Bean
@@ -49,8 +63,18 @@ public class SecurityConfig {
         http.authorizeRequests()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .antMatchers("/api/members/sign-up/email", "/api/members/nickname"
-                        , "/api/members/email-auth", "/api/members/email-authentication", "/api/members/sign-up").permitAll()
+                        , "/api/members/email-auth", "/api/members/email-authentication"
+                        , "/api/members/sign-up", "/api/members/auth/login").permitAll()
+                .antMatchers("/api/admin/**").hasAuthority(ROLE_ADMIN.name())
                 .anyRequest().authenticated();
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(customAuthenticationEntryPoint)
+                .accessDeniedHandler(customAccessDeniedHandler)
+
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
 
         return http.build();
     }
