79ef1b61c873268f5467e7812afe738eab590cd5
grace <grace994084@gmail.com>
2023-05-30 15:04:10 +0900

    feat : access token 재발급 실패에 대한 에러 코드 수정 및 추가

--- src/main/java/com/study/boardserver/global/error/type/MemberAuthErrorCode.java
+++ src/main/java/com/study/boardserver/global/error/type/MemberAuthErrorCode.java
@@ -11,8 +11,9 @@ public enum MemberAuthErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
     FAIL_TO_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "사용자 인증에 실패하였습니다."),
     FAIL_TO_AUTHORIZATION(HttpStatus.FORBIDDEN, "사용자 권한이 없습니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "refresh token이 유효하지 않습니다."),
     NOT_MATCH_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "refresh token이 일치하지 않습니다."),
-    ALREADY_EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "refresh token이 만료되었습니다.");
+    NOT_EXIST_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "refresh token이 존재하지 않습니다.");
     private final HttpStatus status;
     private final String message;
 }
