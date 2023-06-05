d09b06c45d46eb997490fb12abd0b336461ae1d8
grace <grace994084@gmail.com>
2023-06-05 11:18:36 +0900

    feat : 이미지 파일 업로드 시 용량 초과에 대한 에러 코드

--- src/main/java/com/study/boardserver/global/error/type/ImageErrorCode.java
+++ src/main/java/com/study/boardserver/global/error/type/ImageErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ImageErrorCode {
 
     FAIL_TO_UPLOAD_IMAGE(HttpStatus.BAD_REQUEST, "이미지 업로드에 실패하였습니다"),
-    INVALID_IMAGE_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 이미지 형식입니다.");
+    INVALID_IMAGE_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 이미지 형식입니다."),
+    EXCEEDED_IMAGE_SIZE_LIMIT(HttpStatus.PAYLOAD_TOO_LARGE, "업로드 가능한 이미지 크기를 초과하였습니다.");
 
     private final HttpStatus status;
     private final String message;
