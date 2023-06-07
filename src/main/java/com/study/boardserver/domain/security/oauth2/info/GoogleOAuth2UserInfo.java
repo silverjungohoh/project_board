7104f0664f29930851baf13a9a53f9cd5926cc93
grace <grace994084@gmail.com>
2023-06-07 12:58:10 +0900

    feat : 각 소셜로부터 사용자 정보 받아올 수 있도록 OAuth2UserInfo 구현

---
+++ src/main/java/com/study/boardserver/domain/security/oauth2/info/GoogleOAuth2UserInfo.java
@@ -0,0 +1,42 @@
+package com.study.boardserver.domain.security.oauth2.info;
+
+import java.util.Map;
+
+public class GoogleOAuth2UserInfo implements OAuth2UserInfo  {
+
+    private final Map<String, Object> attributes;
+
+    public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("sub").toString();
+    }
+
+    @Override
+    public String getProvider() {
+        return "google";
+    }
+
+    @Override
+    public String getEmail() {
+        return attributes.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attributes.get("name").toString();
+    }
+
+    @Override
+    public String getImageUrl() {
+        return attributes.get("picture").toString();
+    }
+}
---
+++ src/main/java/com/study/boardserver/domain/security/oauth2/info/KakaoOAuth2UserInfo.java
@@ -0,0 +1,47 @@
+package com.study.boardserver.domain.security.oauth2.info;
+
+import java.util.Map;
+
+public class KakaoOAuth2UserInfo implements OAuth2UserInfo {
+
+    private final Map<String, Object> attributes;
+    private final Map<String, Object> attributesAccount;
+    private final Map<String, Object> attributesProfile;
+
+
+    public KakaoOAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+        this.attributesAccount = (Map<String, Object>) attributes.get("kakao_account");
+        this.attributesProfile = (Map<String, Object>) attributes.get("profile");
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public String getEmail() {
+        return attributesAccount.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attributesAccount.get("nickname").toString();
+    }
+
+    @Override
+    public String getImageUrl() {
+        return attributesProfile.get("thumbnail_image_url").toString();
+    }
+}
