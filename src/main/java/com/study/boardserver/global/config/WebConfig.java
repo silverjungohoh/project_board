7a579716aa7f55989e39dff0f585b2acb0698ec2
grace <grace994084@gmail.com>
2023-06-08 15:02:45 +0900

    feat : CORS 설정 위한 WebConfig 생성

---
+++ src/main/java/com/study/boardserver/global/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.study.boardserver.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * cors 설정
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
