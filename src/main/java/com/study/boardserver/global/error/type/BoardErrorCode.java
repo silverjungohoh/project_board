8785f911648a86af6454c664fd960ba38f4f5db5
grace <grace994084@gmail.com>
2023-06-12 16:48:38 +0900

    feat : 게시물 삭제 실패에 대한 에러 코드

---
+++ src/main/java/com/study/boardserver/global/error/type/BoardErrorCode.java
@@ -0,0 +1,16 @@
+package com.study.boardserver.global.error.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum BoardErrorCode {
+
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시물입니다."),
+    CANNOT_DELETE_POST(HttpStatus.FORBIDDEN, "게시물 삭제 권한은 작성자에게 있습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
