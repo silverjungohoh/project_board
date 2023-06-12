5fe510ac1063c9c6c744db55bd436f8cd72cf65e
grace <grace994084@gmail.com>
2023-06-12 17:09:07 +0900

    feat : 게시물 삭제 api 구현

--- src/main/java/com/study/boardserver/domain/board/controller/PostController.java
+++ src/main/java/com/study/boardserver/domain/board/controller/PostController.java
@@ -9,14 +9,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/api/boards")
 @RestController
@@ -33,5 +31,14 @@ public class PostController {
 
         PostWriteResponse response = postService.writePost(userDetails.getMember(), request, files);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @DeleteMapping("/{postId}")
+    @Operation(summary = "게시판 글 삭제")
+    public ResponseEntity<Map<String, String>> deletePost(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                          @PathVariable Long postId) {
+
+        Map<String, String> result = postService.deletePost(userDetails.getMember(), postId);
+        return ResponseEntity.ok(result);
     }
 }
