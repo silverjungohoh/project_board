c5c5ab1da462b76a7e38c53843a6382b72cbb38b
grace <grace994084@gmail.com>
2023-06-12 12:38:23 +0900

    feat : Post entity 생성

---
+++ src/main/java/com/study/boardserver/domain/board/entity/Post.java
@@ -0,0 +1,40 @@
+package com.study.boardserver.domain.board.entity;
+
+import com.study.boardserver.domain.member.entity.Member;
+import com.study.boardserver.global.entity.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "post_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", updatable = false)
+    private Member member;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PostImage> postImages = new ArrayList<>();
+
+    public void addImage(PostImage image) {
+        this.postImages.add(image);
+    }
+}
