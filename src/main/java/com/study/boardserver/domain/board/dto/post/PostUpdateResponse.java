6c0029cbc93b6d667116a6bf5dbdd83c4f14c91f
grace <grace994084@gmail.com>
2023-06-13 13:21:09 +0900

    feat : 게시물 수정 요청 및 응답 dto 생성

---
+++ src/main/java/com/study/boardserver/domain/board/dto/post/PostUpdateResponse.java
@@ -0,0 +1,33 @@
+package com.study.boardserver.domain.board.dto.post;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostUpdateResponse {
+
+    private Long postId;
+
+    private String title;
+
+    private String content;
+
+    private List<PostImageUrlResponse> imageUrls;
+
+    private String nickname;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+}
