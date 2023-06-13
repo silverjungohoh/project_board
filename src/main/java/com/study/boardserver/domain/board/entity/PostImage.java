f583f68e2fbe381e68324420bc4d2003f5d372c9
grace <grace994084@gmail.com>
2023-06-13 14:55:36 +0900

    feat : PostImage id 필드의 컬럼 이름 지정

--- src/main/java/com/study/boardserver/domain/board/entity/PostImage.java
+++ src/main/java/com/study/boardserver/domain/board/entity/PostImage.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 public class PostImage {
 
     @Id
+    @Column(name = "post_image_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
