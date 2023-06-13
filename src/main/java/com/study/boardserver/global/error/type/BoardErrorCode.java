4f0cee463c5896299ab7617538a8e1b7535be6a5
grace <grace994084@gmail.com>
2023-06-13 14:57:20 +0900

    feat : 게시물 이미지가 존재하지 않는 경우에 대한 에러 코드

--- src/main/java/com/study/boardserver/global/error/type/BoardErrorCode.java
+++ src/main/java/com/study/boardserver/global/error/type/BoardErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum BoardErrorCode {
 
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시물입니다."),
-    CANNOT_DELETE_POST(HttpStatus.FORBIDDEN, "게시물 삭제 권한은 작성자에게 있습니다.");
+    CANNOT_DELETE_POST(HttpStatus.FORBIDDEN, "게시물 삭제 권한은 작성자에게 있습니다."),
+    POST_IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 이미지입니다.");
 
     private final HttpStatus status;
     private final String message;
