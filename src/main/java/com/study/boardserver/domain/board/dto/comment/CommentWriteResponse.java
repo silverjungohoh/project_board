4f495efbf28608f59bcd154b888e41922932e43a
grace <grace994084@gmail.com>
2023-06-19 11:40:47 +0900

    feat : 댓글 작성 요청 및 응답 dto 생성

---
+++ src/main/java/com/study/boardserver/domain/board/dto/comment/CommentWriteResponse.java
@@ -0,0 +1,25 @@
+package com.study.boardserver.domain.board.dto.comment;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentWriteResponse {
+
+    private Long commentId;
+
+    private String content;
+
+    private String nickname;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+}
