5bb4f7d397011eab0d2b17fe3a7f1902ab0ac992
grace <grace994084@gmail.com>
2023-06-19 13:19:49 +0900

    feat : 댓글 수정 실패에 대한 에러 코드

--- src/main/java/com/study/boardserver/global/error/type/BoardErrorCode.java
+++ src/main/java/com/study/boardserver/global/error/type/BoardErrorCode.java
@@ -11,7 +11,9 @@ public enum BoardErrorCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시물입니다."),
     CANNOT_DELETE_POST(HttpStatus.FORBIDDEN, "게시물 삭제 권한은 작성자에게 있습니다."),
     POST_IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 이미지입니다."),
-    CANNOT_UPDATE_POST(HttpStatus.FORBIDDEN, "게시물 수정 권한은 작성자에게 있습니다.");
+    CANNOT_UPDATE_POST(HttpStatus.FORBIDDEN, "게시물 수정 권한은 작성자에게 있습니다."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 댓글입니다."),
+    CANNOT_UPDATE_COMMENT(HttpStatus.FORBIDDEN, "댓글 수정 권한은 작성자에게 있습니다.");
 
     private final HttpStatus status;
     private final String message;
