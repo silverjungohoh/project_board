ce298ae4483d4dc3c88d2691d00168fe46b486e5
grace <grace994084@gmail.com>
2023-06-19 13:38:10 +0900

    test : 댓글 수정 기능 구현 테스트 코드

--- src/test/java/com/study/boardserver/domain/board/service/CommentServiceTest.java
+++ src/test/java/com/study/boardserver/domain/board/service/CommentServiceTest.java
@@ -1,5 +1,7 @@
 package com.study.boardserver.domain.board.service;
 
+import com.study.boardserver.domain.board.dto.comment.CommentUpdateRequest;
+import com.study.boardserver.domain.board.dto.comment.CommentUpdateResponse;
 import com.study.boardserver.domain.board.dto.comment.CommentWriteRequest;
 import com.study.boardserver.domain.board.dto.comment.CommentWriteResponse;
 import com.study.boardserver.domain.board.entity.Comment;
@@ -85,6 +87,132 @@ class CommentServiceTest {
         ArgumentCaptor<Comment> commentCaptor = ArgumentCaptor.forClass(Comment.class);
 
         CommentWriteResponse response = commentService.writeComment(member, 1L, request);
+
+        assertEquals(response.getContent(), request.getContent());
+        assertEquals(response.getNickname(), member.getNickname());
+        verify(commentRepository, times(1)).save(commentCaptor.capture());
+    }
+
+    @Test
+    @DisplayName("댓글 수정 실패 - 게시물 없음")
+    void updateComment_Fail_NoPost() {
+
+        Member member = Member.builder()
+                .id(1L)
+                .email("test@test.com")
+                .nickname("nickname")
+                .build();
+
+        CommentUpdateRequest request = CommentUpdateRequest.builder()
+                .content("댓글 수정")
+                .build();
+
+        given(postRepository.existsById(anyLong())).willReturn(false);
+
+        BoardException exception = assertThrows(BoardException.class,
+                () -> commentService.updateComment(member, 1L, 1L, request));
+
+        assertEquals(BoardErrorCode.POST_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("댓글 수정 실패 - 댓글 없음")
+    void updateComment_Fail_NoComment() {
+
+        Member member = Member.builder()
+                .id(1L)
+                .email("test@test.com")
+                .nickname("nickname")
+                .build();
+
+        CommentUpdateRequest request = CommentUpdateRequest.builder()
+                .content("댓글 수정")
+                .build();
+
+        given(postRepository.existsById(anyLong())).willReturn(true);
+        given(commentRepository.findById(anyLong())).willReturn(Optional.empty());
+
+        BoardException exception = assertThrows(BoardException.class,
+                () -> commentService.updateComment(member, 1L, 1L, request));
+
+        assertEquals(BoardErrorCode.COMMENT_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("댓글 수정 실패 - 작성자 아님")
+    void updateComment_Fail_NotMatch() {
+
+        Member member1 = Member.builder()
+                .id(1L)
+                .email("test@test.com")
+                .nickname("nickname1")
+                .build();
+
+        Member member2 = Member.builder()
+                .id(2L)
+                .email("test234@test.com")
+                .nickname("nickname2")
+                .build();
+
+        Post post = Post.builder()
+                .id(1L)
+                .title("제목입니다")
+                .content("내용입니다")
+                .build();
+
+        Comment comment = Comment.builder()
+                .id(1L)
+                .content("댓글")
+                .member(member1)
+                .post(post)
+                .build();
+
+        CommentUpdateRequest request = CommentUpdateRequest.builder()
+                .content("댓글 수정")
+                .build();
+
+        given(postRepository.existsById(anyLong())).willReturn(true);
+        given(commentRepository.findById(anyLong())).willReturn(Optional.of(comment));
+
+        BoardException exception = assertThrows(BoardException.class,
+                () -> commentService.updateComment(member2, 1L, 1L, request));
+
+        assertEquals(BoardErrorCode.CANNOT_UPDATE_COMMENT, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("댓글 수정 성공")
+    void updateComment_Success() {
+
+        Member member = Member.builder()
+                .id(1L)
+                .email("test@test.com")
+                .nickname("nickname")
+                .build();
+
+        Post post = Post.builder()
+                .id(1L)
+                .title("제목입니다")
+                .content("내용입니다")
+                .build();
+
+        Comment comment = Comment.builder()
+                .id(1L)
+                .content("댓글")
+                .member(member)
+                .post(post)
+                .build();
+
+        CommentUpdateRequest request = CommentUpdateRequest.builder()
+                .content("댓글 수정")
+                .build();
+
+        given(postRepository.existsById(anyLong())).willReturn(true);
+        given(commentRepository.findById(anyLong())).willReturn(Optional.of(comment));
+
+        ArgumentCaptor<Comment> commentCaptor = ArgumentCaptor.forClass(Comment.class);
+
+        CommentUpdateResponse response = commentService.updateComment(member, 1L, 1L, request);
 
         assertEquals(response.getContent(), request.getContent());
         assertEquals(response.getNickname(), member.getNickname());
