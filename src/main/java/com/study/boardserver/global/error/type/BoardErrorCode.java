c99e4a4845ab3332d36d7b2a53f0fe9e932c72f8
grace <grace994084@gmail.com>
2023-06-23 12:09:38 +0900

    feat : 좋아요 등록 실패에 대한 에러 코드

--- src/main/java/com/study/boardserver/global/error/type/BoardErrorCode.java
+++ src/main/java/com/study/boardserver/global/error/type/BoardErrorCode.java
@@ -14,7 +14,9 @@ public enum BoardErrorCode {
     CANNOT_UPDATE_POST(HttpStatus.FORBIDDEN, "게시물 수정 권한은 작성자에게 있습니다."),
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 댓글입니다."),
     CANNOT_UPDATE_COMMENT(HttpStatus.FORBIDDEN, "댓글 수정 권한은 작성자에게 있습니다."),
-    CANNOT_DELETE_COMMENT(HttpStatus.FORBIDDEN, "댓글 삭제 권한은 작성자에게 있습니다.");
+    CANNOT_DELETE_COMMENT(HttpStatus.FORBIDDEN, "댓글 삭제 권한은 작성자에게 있습니다."),
+    CANNOT_PUSH_HEART(HttpStatus.BAD_REQUEST, "자신의 게시물에 좋아요를 누를 수 없습니다."),
+    ALREADY_PUSH_HEART(HttpStatus.BAD_REQUEST, "이미 게시물에 좋아요를 눌렀습니다.");
 
     private final HttpStatus status;
     private final String message;
